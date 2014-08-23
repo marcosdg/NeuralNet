b297ad428c1f53a3e93f1558e9d576a7dd18de78
mancas <mancas.91@gmail.com>
2014-08-23 11:22:17 +0200

    logger

--- NeuralNet/src/core/utils/Log.java
+++ NeuralNet/src/core/utils/Log.java
@@ -3,8 +3,12 @@ package core.utils;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Log {
 
@@ -15,10 +19,20 @@ public class Log {
 		String pathToConfigFile = new StringBuilder(pathToWorkSpace())
 							        .append("/src/core/utils/log.properties")
 							        .toString();
+		String pathToLog = new StringBuilder(pathToUserHome())
+									.append("/")
+									.append(className)
+									.append(".log")
+									.toString();
 		logger = Logger.getLogger(className);
 		try {
 			LogManager.getLogManager().readConfiguration(
 			        new FileInputStream(pathToConfigFile));
+			//FileHandler file name with max size and number of log files limit
+            Handler fileHandler = new FileHandler(pathToLog);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.addHandler(fileHandler);
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -37,10 +51,14 @@ public class Log {
 		return System.getProperty("user.dir");
 	}
 
+	private String pathToUserHome() {
+		return System.getProperty("user.home");
+	}
+
 	public static void log(String msg)
 	{
 		if (logger != null) {
-			logger.log(logger.getLevel(), msg);
+			logger.log(Level.INFO, msg);
 		}
 	}
 
---
+++ NeuralNet/src/test/TestLog.java
@@ -0,0 +1,13 @@
+package test;
+
+import core.utils.Log;
+
+public class TestLog {
+
+	public static void main(String[] args) {
+		Log log = new Log(TestLog.class.getName());
+		log.log("Hello!");
+		log.log("This is an example");
+	}
+
+}
